978bedbf314419675340513f1353bfff03bfb650
Grant Falkner <gjfalkner@mymail.mines.edu>
2021-05-25 15:48:30 -0600

    cleaned up utility functions

--- NorthAmerica/util.py
+++ NorthAmerica/util.py
@@ -31,7 +31,10 @@ def load_dfs(path, allowed_extensions=[".shp", ".zip", ".csv", ".gdb"]):
             extension = os.path.splitext(file_name)[1]
             if allowed_extensions.count(extension) != 0:
                 try:
-                    dfs[file] = gpd.read_file(file_name)
+                    df = gpd.read_file(file_name)
+                    columns = [i for i in df.columns if "Lat" in i or "lat" in i]
+                    print(columns)
+                    dfs[file] = df
                 except:
                     raise Exception(f"Error loading dataframes from {path}")
     return dfs
@@ -41,16 +44,27 @@ def get_traces_from_dfs(dfs):
     colors = ["red", "yellow", "green", "pink", "orange", "purple"]
     counter = 0
     for df_name, df in dfs.items():
+        print(df_name)
         print(df.head)
+        print()
         if "geometry" in df:
             if isinstance(df.geometry[0], shapely.geometry.linestring.LineString) \
                     or isinstance(df.geometry[0], shapely.geometry.multilinestring.MultiLineString):
                 lats, lons = lat_lon_lists_from_df(df)
-                traces.append(go.Scattermapbox(name=df_name, visible="legendonly", lon=lons, lat=lats, mode='lines', line=dict(width=1, color=colors[counter%len(colors)])))
+                traces.append(go.Scattermapbox(name=df_name,
+                                               visible="legendonly",
+                                               lon=lons, lat=lats,
+                                               mode='lines',
+                                               line=dict(width=1, color=colors[counter%len(colors)])))
                 counter += 1
             elif isinstance(df.geometry[0], shapely.geometry.point.Point):
-                df['lon'] = df['geometry'].x
-                df['lat'] = df['geometry'].y
-                traces.append(go.Scattermapbox(name=df_name, visible="legendonly", lat=df['lat'], lon=df['lon'], marker ={'color': colors[counter%len(colors)], 'size': 5, 'opacity': 0.6}))
+                traces.append(go.Scattermapbox(name=df_name,
+                                               visible="legendonly",
+                                               lat=df.geometry.y, lon=df.geometry.x,
+                                               marker={'color': colors[counter%len(colors)], 'size': 5, 'opacity': 0.6}))
                 counter += 1
+            elif df.geometry[0] is None:
+                continue
+            else:
+                raise Exception(f"The geometry in {df_name} is not supported")
     return traces
